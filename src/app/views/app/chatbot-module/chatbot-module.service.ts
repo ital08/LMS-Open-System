93a390802410ab20d3b8d7045fdbac41be4413ac
Ricardo Chariarse <ricardo.chariarse@gestionysistemas.com>
2023-07-22 11:01:16 -0500

    chatbot module

--- src/app/views/app/chatbot-module/chatbot-module.service.ts
+++ src/app/views/app/chatbot-module/chatbot-module.service.ts
@@ -8,15 +8,8 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class ChatbotModuleService {
-<<<<<<< HEAD
 
   private apiKey = environment.chatGptApiKey;
-=======
-  //private apiKey = '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';
-  // private apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
-
-  private apiKey = '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';
->>>>>>> 6d8841285c64a7633a6444b48b88305284571cb1
   private apiUrl = 'https://api.openai.com/v1/completions';
 
   async getChatbotResponse(inputText: string): Promise<string> {
